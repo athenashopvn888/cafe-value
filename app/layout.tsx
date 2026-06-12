d2954bc4b8b27157d00364f166546ff0f3a1a659
Alexander <sixttrading@gmail.com>
2026-06-12 10:29:06 +0700

    add google analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,6 +135,17 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PFWL6WGNM6"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PFWL6WGNM6');
+            `
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
