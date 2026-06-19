32e8dfdec3e5144344ca30b9a77139cb85b69aaf
Alexander <sixttrading@gmail.com>
2026-06-19 12:03:57 +0700

    Add GBP local SEO landing pages with manual overrides

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -74,6 +74,7 @@ export default function Footer() {
               <Link href="/info/cheap-weed-toronto">Cheap Weed Toronto</Link>
               <Link href="/info/native-cigarettes-toronto">Native Cigarettes</Link>
               <Link href="/info/weed-store-near-mississauga">Weed Store Near Mississauga</Link>
+              <Link href="/weed-dispensary-toronto/">Cafe Value Weed Dispensary in Toronto</Link>
               <Link href="/contact">Contact Us</Link>
               <a
                 href="https://maps.app.goo.gl/6VfAL3aJzuRDL3gbA"
